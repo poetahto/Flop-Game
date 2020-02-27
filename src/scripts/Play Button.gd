b876226fee69c2f0d9e62e4cc73b5d59083833f9
poetahto <epic.potat@gmail.com>
2020-02-26 23:39:03 -0800

    added buttons and a menu workflow

---
+++ src/scripts/Play Button.gd
@@ -0,0 +1,5 @@
+extends Button
+
+func _pressed() -> void:
+	SceneChanger.change_scene("res://src/levels/TestLevel.tscn")
+
---
+++ src/scripts/reset button.gd
@@ -0,0 +1,4 @@
+extends Button
+
+func _pressed() -> void:
+	SceneChanger.change_scene("res://src/levels/TestLevel.tscn")
